a5592f584b6ba05a300b7ef8e44f384e9e5f9755
Rachel Haas <rachelhaas@ufl.edu>
2018-12-02 14:48:23 -0500

    Added an object

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,10 @@ var members = [
   'Rami Jaffee',
   'Chris Shiflett'
 ];
+
+//Object
+var tour = {
+  chicago: 'July 29th & 30th, 2018',
+  seattle: 'September 1st, 2018',
+  edmonton: 'September 4th, 2018'
+};
